6ca9d9647097f67aad16b6612779d1a2ba9e6f27
David Robert de Souza <david_robert97@outlook.com>
2022-10-18 20:34:06 -0300

    Criado cards

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+  return(
+    <div className='colaborador'>
+      <div className='cabecalho'>
+        <img src='http://github.com/davidrobertdesouza.png' alt='David Robert'/>
+      </div>
+      <div className='rodape'>
+        <h4>David Robert</h4>
+        <h5>Aluno</h5>
+      </div>
+    </div>
+  )
+}
+
+export default Colaborador
